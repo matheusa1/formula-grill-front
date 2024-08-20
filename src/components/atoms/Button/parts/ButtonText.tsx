6d8a6318bf980776d4c77452aab04c1f59ca4746
Matheus Andrade <2002matheus@gmail.com>
2024-08-20 16:29:43 -0300

    fix: :bug: arrumado import

--- src/components/atoms/Button/parts/ButtonText.tsx
+++ src/components/atoms/Button/parts/ButtonText.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { FC } from 'react'
-import { TButtonText } from '../type'
+import { TButtonText } from '../types'
 
 export const ButtonText: FC<TButtonText> = ({ children, ...rest }) => {
   return <p {...rest}>{children}</p>
