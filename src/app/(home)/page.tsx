6de2a46d8921cd31b3727b30dab8f4b7effbc58f
Matheus Andrade <2002matheus@gmail.com>
2024-08-30 20:16:08 -0300

    feat: 🎸 carouesel
    finalizado carousel
    
    ✅ Closes: #18

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,11 +1,13 @@
 import { FC } from 'react'
 import Reservations from './reservations'
 import InitialPage from './initialPage'
+import FoodCarousel from './FoodCarousel'
 
 const Home: FC = () => {
   return (
     <div className={''}>
       <InitialPage />
+      <FoodCarousel />
       <Reservations />
     </div>
   )
