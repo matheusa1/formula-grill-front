ace84ebd459bddbac2212521eea6da8eefefca2e
Matheus Andrade <2002matheus@gmail.com>
2024-08-30 21:27:19 -0300

    feat: 🎸 footer
    Footer feito
    
    ✅ Closes: #20

--- src/components/organisms/Footer/index.tsx
+++ src/components/organisms/Footer/index.tsx
@@ -1,13 +1,28 @@
 import Image from 'next/image'
 import { FC } from 'react'
 import secondLogo from '@/assets/svg/secondLogo.svg'
+import FooterColumn from '@/components/molecules/FooterColumn'
+import AboutCards from '@/components/molecules/AboutCards'
+import { aboutCardsData } from '@/app/(home)/about'
 
 const Footer: FC = () => {
+  const data = aboutCardsData
+
   return (
-    <div className={'flex flex-col bg-black bg-carbon p-5'}>
-      <h1>Footer</h1>
-      <Image alt={'logo'} src={secondLogo} />
-      <h1>Footer</h1>
+    <div
+      className={
+        'flex flex-col items-center gap-10 bg-black bg-carbon p-5 lg:flex-row lg:justify-evenly lg:py-40'
+      }
+    >
+      <FooterColumn>
+        <AboutCards info={data[0].info} subInfo={data[0].subInfo} />
+        <AboutCards info={data[2].info} subInfo={data[1].subInfo} />
+      </FooterColumn>
+      <Image alt={'logo'} src={secondLogo} className="w-full max-w-80" />
+      <FooterColumn>
+        <AboutCards info={data[0].info} subInfo={data[0].subInfo} />
+        <AboutCards info={data[2].info} subInfo={data[1].subInfo} />
+      </FooterColumn>
     </div>
   )
 }
