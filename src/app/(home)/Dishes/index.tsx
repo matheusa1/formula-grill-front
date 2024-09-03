254db590a2dc4620a470601f4d97d49ad7bc6a0e
Matheus Andrade <2002matheus@gmail.com>
2024-09-03 20:08:26 -0300

    feat: 🎸 listagem de pratos feita

---
+++ src/app/(home)/Dishes/index.tsx
@@ -0,0 +1,40 @@
+'use client'
+
+import CategoriesList from '@/components/organisms/CategoriesList'
+import { useAuth } from '@/context/AuthContext'
+import { getCategories } from '@/services/api'
+import { TCategoriesResponse } from '@/types/categoriesTypes'
+import { FC } from 'react'
+import { useQuery } from 'react-query'
+
+const Dishes: FC = () => {
+  const { token } = useAuth()
+
+  const { data: categoriesList } = useQuery({
+    queryKey: 'dishes',
+    queryFn: async () => {
+      const response = await getCategories(token!)
+        .then((res) => res)
+        .catch(() => [] as TCategoriesResponse[])
+
+      return response
+    },
+  })
+
+  return (
+    <div className={'flex w-full flex-col gap-5 p-4 text-white'}>
+      <header className="flex flex-col gap-5 text-center text-xl font-bold">
+        <h1>Cardápio</h1>
+        <p>Do grid de largada ao último garfo, uma experiência única.</p>
+      </header>
+
+      <main className="flex flex-col gap-10">
+        {categoriesList?.map((category) => (
+          <CategoriesList key={category.id} data={category} />
+        ))}
+      </main>
+    </div>
+  )
+}
+
+export default Dishes
